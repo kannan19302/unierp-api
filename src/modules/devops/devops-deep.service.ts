49e6ef2c0f2c30539d41624f4236c78a8d60c313
kannan19302 <kannan19302@gmail.com>
2026-08-04 08:08:58 +0530

    refactor(api): rewire devops for the platform split

--- src/modules/devops/devops-deep.service.ts
+++ src/modules/devops/devops-deep.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException, Logger } from "@nestjs/common";
 import { prisma } from "@unerp/database";
+import { idpClient as idpPrisma } from "@/common/idp-client";
 import {
   pipelines,
   configMaps,
